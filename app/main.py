caec5251b688dfac7b882dad260a655ab2d75438
jeevanvb24 <jeevan.cb@suketa.in>
2026-06-07 17:20:57 +0530

    just pushed all the changes

--- app/main.py
+++ app/main.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "Hello CI/CD"}
+    return {"message": "Hello CI/CD just deployed my 1st FastAPI app with GitHub Actions and Docker!"}
